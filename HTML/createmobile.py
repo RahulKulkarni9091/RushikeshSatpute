fa73c63dd8a5a13ea233466f57d57f3ce0e2121b
AntiwareTechHub <91723373+AntiwareTechHub@users.noreply.github.com>
2021-10-04 07:40:33 +0530

    Added create script

---
+++ HTML/createmobile.py
@@ -0,0 +1,10 @@
+import sys
+with open("test.txt", 'w') as            
+    sys.stdout:
+    from random import randintdef
+    random_with_N_digits(n):       
+    range_start = 10**(n-1) range_end = (10**n)-1
+    return randint(range_start, 
+    range_end) for mciNumbers in 
+    range(0,100):	print('0912', 
+    random_with_N_digits(7))
